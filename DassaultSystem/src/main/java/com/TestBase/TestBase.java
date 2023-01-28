f7c46fa4f53d010f0e123afc04a7b915457d8a00
priya-mane1203 <121284285+priya-mane1203@users.noreply.github.com>
2023-01-28 15:21:44 +0530

    Basic Project Structure
    Hi all,
    I have create basic project structure please import it and start your working on it.

---
+++ DassaultSystem/src/main/java/com/TestBase/TestBase.java
@@ -0,0 +1,37 @@
+package com.TestBase;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestBase {
+	public static WebDriver driver;
+	
+	public void drivers()
+	{
+	String browser="Chrome";
+	if(browser.equalsIgnoreCase("Chrome"))
+	{
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+	}
+	else if(browser.equalsIgnoreCase("Firefox"))
+	{
+		WebDriverManager.firefoxdriver().setup();
+		driver=new FirefoxDriver();
+	}
+	
+	driver.manage().window().maximize();
+	driver.manage().deleteAllCookies();
+	driver.get("https://phptravels.com/");
+	}
+	
+	public void closeBrowser()
+	{
+		driver.quit();
+		
+	}
+}
+
